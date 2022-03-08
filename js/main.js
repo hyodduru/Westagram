174295c6d1cb785f0fbf89fa5983baf81a97f1c8
hyodduru <hoje15v@gmail.com>
2022-03-08 17:03:17 +0900

    JS : adding comments

--- js/main.js
+++ js/main.js
@@ -5,11 +5,38 @@ const idInput = document.getElementById("id");
 const passwordInput = document.getElementById("password");
 const loginBtn = document.getElementById("login-btn");
 
+// Main Elements
+const commentInput = document.getElementById("comment-input");
+const commentForm = document.getElementById("comment-form");
+const comments = document.getElementById("comments");
+let id = "hyodduru";
+
+function createComment(comment) {
+  const li = document.createElement("li");
+  li.innerHTML = `<strong>${id}</strong> ${comment}`;
+  li.className = "comment";
+  comments.appendChild(li);
+}
+
+function submitComment(e) {
+  e.preventDefault();
+  const newComment = commentInput.value;
+  createComment(newComment);
+  commentInput.value = "";
+}
+
+commentForm.addEventListener("submit", submitComment);
+commentForm.addEventListener("input", () => {
+  commentForm.classList.add("activate");
+  if (commentInput.value === "") commentForm.classList.remove("activate");
+});
+
 // Login handling
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
   if (id.length === 0 || password.length === 0) return;
 
+  id = idInput.value;
   //Clear input
   idInput.value = passwordInput.value = "";
   //Hide Login Form
